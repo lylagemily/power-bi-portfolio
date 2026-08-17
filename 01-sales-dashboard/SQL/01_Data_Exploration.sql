5aec859dcf0f8c010b478c6db3795ddbd91b9345
lylagemily <lylagemily512@gmail.com>
2026-08-17 15:46:53 +0800

    Update 01_Data_Exploration.sql

--- 01-sales-dashboard/SQL/01_Data_Exploration.sql
+++ 01-sales-dashboard/SQL/01_Data_Exploration.sql
@@ -1,1 +1,37 @@
+USE AdventureWorks2025;
+GO
 
+-- =============================================
+-- AdventureWorks BI Portfolio
+-- Sales Dashboard
+-- 01 - Data Exploration
+-- =============================================
+
+-- Check current database
+SELECT
+    DB_NAME() AS DatabaseName;
+
+
+-- Explore Sales Order Header
+SELECT TOP 10 *
+FROM Sales.SalesOrderHeader;
+
+
+-- Explore Sales Order Detail
+SELECT TOP 10 *
+FROM Sales.SalesOrderDetail;
+
+
+-- Explore Products
+SELECT TOP 10 *
+FROM Production.Product;
+
+
+-- Explore Customers
+SELECT TOP 10 *
+FROM Sales.Customer;
+
+
+-- Explore Employees
+SELECT TOP 10 *
+FROM HumanResources.Employee;
