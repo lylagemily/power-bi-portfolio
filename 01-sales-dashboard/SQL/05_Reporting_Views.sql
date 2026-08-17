5a29932be810eb5da7073c9e0e81637744ed6b0c
lylagemily <lylagemily512@gmail.com>
2026-08-17 15:50:31 +0800

    Update 05_Reporting_Views.sql

--- 01-sales-dashboard/SQL/05_Reporting_Views.sql
+++ 01-sales-dashboard/SQL/05_Reporting_Views.sql
@@ -1,1 +1,49 @@
+USE AdventureWorks2025;
+GO
 
+-- =============================================
+-- AdventureWorks BI Portfolio
+-- Sales Dashboard
+-- 05 - Reporting Views
+-- =============================================
+
+CREATE OR ALTER VIEW dbo.vw_SalesDetail
+AS
+
+SELECT
+    h.SalesOrderID,
+    h.OrderDate,
+    h.CustomerID,
+    h.SalesPersonID,
+    h.TerritoryID,
+
+    d.SalesOrderDetailID,
+    d.ProductID,
+    d.OrderQty,
+    d.UnitPrice,
+    d.UnitPriceDiscount,
+
+    p.Name AS ProductName,
+    p.ProductNumber,
+    p.Color,
+    p.Size,
+    p.ProductLine,
+
+    d.OrderQty * d.UnitPrice AS GrossSales,
+
+    d.OrderQty *
+    d.UnitPrice *
+    d.UnitPriceDiscount AS DiscountAmount,
+
+    d.OrderQty *
+    d.UnitPrice *
+    (1 - d.UnitPriceDiscount) AS NetSales
+
+FROM Sales.SalesOrderHeader AS h
+
+INNER JOIN Sales.SalesOrderDetail AS d
+    ON h.SalesOrderID = d.SalesOrderID
+
+INNER JOIN Production.Product AS p
+    ON d.ProductID = p.ProductID;
+GO
